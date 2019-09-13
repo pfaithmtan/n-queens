8f83d23a44ad3dd18d3915a72375ad2ff1f3082c
pfaithmtan <pfaithmtan@gmail.com>
2019-09-12 20:51:02 -0700

    Attempt countNRooksSolutions
    - Start implementation

--- src/solvers.js
+++ src/solvers.js
@@ -38,7 +38,22 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  const board = new Board({n: n});
+  const boards = [];
+
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      const newBoard = new Board({n: n});
+
+      newBoard.togglePiece(row, col);
+      boards.push(newBoard);
+    }
+  }
+
+  for (var i = 0; i < boards.length; i++) {
+
+  }
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
